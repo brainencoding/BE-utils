bccb5a03aaaf36918a9f7ce1edf9d2bcebc0b562
Adamets Vladislav <v.adamets@consult-info.ru>
2021-05-29 01:09:14 +0200

    upd: add object assign

--- src/BE-utils.js
+++ src/BE-utils.js
@@ -567,6 +567,42 @@ let BE;
 	BE.object.observer = Observer;
 
 	/**
+	 *
+	 * @param target {Object}
+	 * @param source {Object}
+	 * @return {Object}
+	 * */
+	function ObjectAssign(target, source) {
+		if (target === undefined || target === null) {
+			throw new TypeError('Cannot convert first argument to object');
+		}
+
+		let to = Object(target);
+		
+		for (let i = 1; i < arguments.length; i++) {
+			const nextSource = arguments[i];
+			
+			if (nextSource === undefined || nextSource === null) {
+				continue;
+			}
+
+			const keysArray = Object.keys(Object(nextSource));
+
+			for (let nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+				let nextKey = keysArray[nextIndex];
+				let desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+				
+				if (desc !== undefined && desc.enumerable) {
+					to[nextKey] = nextSource[nextKey];
+				}
+			}
+		}
+		
+		return to;
+	}
+	BE.object.assign = ObjectAssign;
+
+	/**
 	 * Objects comparing
 	 *
 	 * 	Example:
